d5ba19743de96ea35e1c58f565125e7c152da1f5
Nathan Mar <nathanymar@gmail.com>
2026-05-23 18:34:20 -0700

    change checksum options in s3client

--- server/src/config/s3Client.ts
+++ server/src/config/s3Client.ts
@@ -9,7 +9,9 @@ const s3Client = new S3Client({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-  }
+  },
+  requestChecksumCalculation: "WHEN_REQUIRED",
+  responseChecksumValidation: "WHEN_REQUIRED"
 });
 
 export default s3Client;
